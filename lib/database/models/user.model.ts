f0da9a5ca90413f5cddbe4e0332bda42f6310013
鈴木裕毅 <yu8867ki0217@gmail.com>
2024-08-18 22:58:53 +0900

    init

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,4 @@
-import { Schema, model, models, Document } from "mongoose";
-
-export interface IUser extends Document {
-  _id: string;
-  clerkId: string;
-  email: string;
-  username: string;
-  firstName: string;
-  lastName: string;
-  photo: string;
-}
+import { Schema, model, models } from "mongoose";
 
 const UserSchema = new Schema(
   {
